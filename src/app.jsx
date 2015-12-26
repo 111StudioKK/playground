f14683ff7e532368a8e84f7a4d3d19338e48e54a
marc <contact@macartiant.com>
2015-12-27 05:34:59 +0900

    Initial commit
    Project reboot with proper react dev env

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
 var React = require('react');
+var Button = require('./button');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
-});
+var options = {
+  title:"Choose an option",
+  items:[
+    "option 1",
+    "option 2",
+    "option 3"
+  ]
+};
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(Button, options);
 ReactDOM.render(element, document.querySelector('.container'));
