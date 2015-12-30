a4211d45b686493a1cc4a4b43197fec957d1db19
marc <contact@macartiant.com>
2015-12-31 08:26:32 +0900

    Added Admin interface
    edit/delete item

--- src/app.jsx
+++ src/app.jsx
@@ -12,24 +12,37 @@ var ListEdit = require('./listedit');
 var app = React.createClass({
   mixins: [ ReactFire],
   componentWillMount: function(){
-    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    var FBase = new Firebase(rootUrl + 'items/')
+    this.bindAsObject(FBase, 'items');
+    FBase.on("value",this.DataLoaded)
+  },
+  DataLoaded:function(){
+    this.setState({DataReady:true})
   },
   getInitialState:function(){
     return {
-      items: {}
+      items: {},
+      DataReady:false
     };
   },
   render:function(){
+    console.log(this.state.DataReady);
     return <div className="row panel panel-default">
       <div className="col-md-8 cold-md-offset-2">
         <h2 className="text-center">
-          Dropbox powered by Firebase and React
+          Dropbox editor powered by Firebase and React
         </h2>
         <Header base={this.firebaseRefs.items}/>
-        <ListEdit items={this.state.items} />
+        <hr />
+          <div className={"content " + (this.state.DataReady? 'loaded':'')} >
+            <ListEdit items={this.state.items} />
+          </div>
+          <hr />
+            <Dropdown items={this.FireBaseToItemsArray()} />
       </div>
     </div>
   },
+
 FireBaseToItemsArray:function(){
     if (this.state.items && Object.keys(this.state.items).lenght ===0){
       return "";
--- src/listedit.jsx
+++ src/listedit.jsx
@@ -1,22 +1,22 @@
 var React = require('react');
+var Item = require ('./item');
 
 module.exports = React.createClass({
   render:function(){
-
-    return <ul>{this.datadisplay()}</ul>
+      return <div>{this.datadisplay()}</div>
   },
   datadisplay:function(){
     if (!this.props.items||Object.keys(this.props.items).lenght ===0){
-      console.log("test");
       return <h1>nothing to display</h1>
       }
       else {
         var children = [];
         for (var key in this.props.items){
           children.push(
-            <li>
-              {this.props.items[key].text}
-            </li>
+            <Item
+              text={this.props.items[key].text}
+              key = {key}>
+            </Item>
           );
         }
         return children;
